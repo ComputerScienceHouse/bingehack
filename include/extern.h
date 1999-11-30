5eb78af6204a3daa15bc5cef905f0209020ca5a0
dean <dean>
1999-11-30 04:05:15 +0000

    From: Janet Walz <jwalz@radix.net> Subject: bit
    "questpgr.c", line 266: warning: illegal combination of pointer and integer, op =

--- include/extern.h
+++ include/extern.h
@@ -1454,6 +1454,7 @@ E const char *NDECL(a_gname);
 E const char *FDECL(a_gname_at, (XCHAR_P x,XCHAR_P y));
 E const char *FDECL(align_gname, (ALIGNTYP_P));
 E const char *FDECL(halu_gname, (ALIGNTYP_P));
+E const char *FDECL(align_gtitle, (ALIGNTYP_P));
 E void FDECL(altar_wrath, (int,int));
 
 
