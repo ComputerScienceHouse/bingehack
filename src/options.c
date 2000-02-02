e8c8b13396095ba3351ee19553172f201445f983
dean <dean>
2000-02-02 18:48:41 +0000

    From: Janet Walz <jwalz@radix.net> Subject: options reformatting
    Actual change:  one Mac-only option from undefined FILE_ONLY to defined
    SET_IN_FILE.  Otherwise formatting to avoid line wraps.
    
    Modified Files:
    	src/options.c

--- src/options.c
+++ src/options.c
@@ -184,10 +184,11 @@ static struct Bool_Opt
 
 /* compound options, for option_help() and external programs like Amiga
  * frontend */
-#define SET_IN_FILE	0 /* config file option only, not visible in game or via program */
-#define SET_VIA_PROG	1 /* may be set via extern options processor, not seen in game   */
-#define DISP_IN_GAME	2 /* may be set via extern options processor, displayed in game  */
-#define SET_IN_GAME	3 /* may be set via extern options processor or set in the game  */
+#define SET_IN_FILE	0 /* config file option only, not visible in game
+			   * or via program */
+#define SET_VIA_PROG	1 /* may be set via extern program, not seen in game */
+#define DISP_IN_GAME	2 /* may be set via extern program, displayed in game */
+#define SET_IN_GAME	3 /* may be set via extern program or set in the game */
 static struct Comp_Opt
 {
 	const char *name, *descr;
@@ -196,85 +197,103 @@ static struct Comp_Opt
 			 * occasionally maximum reasonable size for
 			 * typing when game maintains information in
 			 * a different format */
-	int optflags;	/* SET_IN_FILE, SET_VIA_PROG, DISP_IN_GAME, or SET_IN_GAME  */
+	int optflags;
 } compopt[] = {
 	{ "align",    "your starting alignment (lawful, neutral, or chaotic)",
-						8, DISP_IN_GAME},
+						8, DISP_IN_GAME },
 #ifdef MAC
-	{ "background", "the color of the background (black or white),",
+	{ "background", "the color of the background (black or white)",
 						6, SET_IN_FILE },
 #endif
 	{ "catname",  "the name of your (first) cat (e.g., catname:Tabby)",
-						PL_PSIZ , DISP_IN_GAME},
+						PL_PSIZ, DISP_IN_GAME },
 	{ "disclose", "the kinds of information to disclose at end of game",
-						sizeof(flags.end_disclose), SET_IN_GAME },
+						sizeof(flags.end_disclose),
+						SET_IN_GAME },
 	{ "dogname",  "the name of your (first) dog (e.g., dogname:Fang)",
-						PL_PSIZ , DISP_IN_GAME},
+						PL_PSIZ, DISP_IN_GAME },
 	{ "dungeon",  "the symbols to use in drawing the dungeon map",
-						MAXDCHARS+1, SET_IN_FILE},
+						MAXDCHARS+1, SET_IN_FILE },
 	{ "effects",  "the symbols to use in drawing special effects",
-						MAXECHARS+1, SET_IN_FILE},
+						MAXECHARS+1, SET_IN_FILE },
 #ifdef MAC
-	{ "fontmap", "the font to use in the map window,", 40 , SET_IN_FILE},
-	{ "fontmessage", "the font to use in the message window,", 40, SET_IN_FILE},
-	{ "fonttext", "the font to use in text windows,", 40 , FILE_ONLY},
+	{ "fontmap", "the font to use in the map window", 40, SET_IN_FILE },
+	{ "fontmessage", "the font to use in the message window",
+						40, SET_IN_FILE },
+	{ "fonttext", "the font to use in text windows", 40, SET_IN_FILE },
 #endif
 	{ "fruit",    "the name of a fruit you enjoy eating",
-						PL_FSIZ , SET_IN_GAME},
+						PL_FSIZ, SET_IN_GAME },
 	{ "gender",   "your starting gender (male or female)",
 						8, DISP_IN_GAME },
 	{ "horsename", "the name of your (first) horse (e.g., horsename:Silver)",
-						PL_PSIZ, DISP_IN_GAME},
-	{ "menustyle", "user interface for object selection", MENUTYPELEN, SET_IN_GAME },
-	{ "menu_deselect_all", "deselect all items in a menu", 4, SET_IN_FILE},
-	{ "menu_deselect_page", "deselect all items on this page of a menu", 4, SET_IN_FILE},
-	{ "menu_first_page", "jump to the first page in a menu", 4, SET_IN_FILE},
-	{ "menu_invert_all", "invert all items in a menu", 4, SET_IN_FILE},
-	{ "menu_invert_page", "invert all items on this page of a menu", 4, SET_IN_FILE},
-	{ "menu_last_page", "jump to the last page in a menu", 4, SET_IN_FILE},
-	{ "menu_next_page", "goto the next menu page", 4, SET_IN_FILE},
-	{ "menu_previous_page", "goto the previous menu page", 4, SET_IN_FILE},
-	{ "menu_search", "search for a menu item", 4, SET_IN_FILE},
-	{ "menu_select_all", "select all items in a menu", 4, SET_IN_FILE},
-	{ "menu_select_page", "select all items on this page of a menu", 4, SET_IN_FILE},
-	{ "monsters", "the symbols to use for monsters", MAXMCLASSES, SET_IN_FILE},
-	{ "msghistory", "number of top line messages to save", 5 , DISP_IN_GAME},
-	{ "name",     "your character's name (e.g., name:Merlin-W)", PL_NSIZ, DISP_IN_GAME },
-	{ "objects",  "the symbols to use for objects", MAXOCLASSES, SET_IN_FILE },
+						PL_PSIZ, DISP_IN_GAME },
+	{ "menustyle", "user interface for object selection",
+						MENUTYPELEN, SET_IN_GAME },
+	{ "menu_deselect_all", "deselect all items in a menu", 4, SET_IN_FILE },
+	{ "menu_deselect_page", "deselect all items on this page of a menu",
+						4, SET_IN_FILE },
+	{ "menu_first_page", "jump to the first page in a menu",
+						4, SET_IN_FILE },
+	{ "menu_invert_all", "invert all items in a menu", 4, SET_IN_FILE },
+	{ "menu_invert_page", "invert all items on this page of a menu",
+						4, SET_IN_FILE },
+	{ "menu_last_page", "jump to the last page in a menu", 4, SET_IN_FILE },
+	{ "menu_next_page", "goto the next menu page", 4, SET_IN_FILE },
+	{ "menu_previous_page", "goto the previous menu page", 4, SET_IN_FILE },
+	{ "menu_search", "search for a menu item", 4, SET_IN_FILE },
+	{ "menu_select_all", "select all items in a menu", 4, SET_IN_FILE },
+	{ "menu_select_page", "select all items on this page of a menu",
+						4, SET_IN_FILE },
+	{ "monsters", "the symbols to use for monsters",
+						MAXMCLASSES, SET_IN_FILE },
+	{ "msghistory", "number of top line messages to save",
+						5, DISP_IN_GAME },
+	{ "name",     "your character's name (e.g., name:Merlin-W)",
+						PL_NSIZ, DISP_IN_GAME },
+	{ "objects",  "the symbols to use for objects",
+						MAXOCLASSES, SET_IN_FILE },
 	{ "packorder", "the inventory order of the items in your pack",
 						MAXOCLASSES, SET_IN_GAME },
 #ifdef CHANGE_COLOR
 	{ "palette",  "palette (00c/880/-fff is blue/yellow/reverse white)",
-						15 , SET_IN_GAME},
+						15 , SET_IN_GAME },
 # if defined(MAC)
-	{ "hicolor",  "same as palette, only order is reversed", 15, SET_IN_FILE },
+	{ "hicolor",  "same as palette, only order is reversed",
+						15, SET_IN_FILE },
 # endif
 #endif
-	{ "pettype",  "your preferred initial pet type", 4 , DISP_IN_GAME },
-	{ "pickup_burden",  "maximum burden picked up before prompt", 20, SET_IN_GAME },
+	{ "pettype",  "your preferred initial pet type", 4, DISP_IN_GAME },
+	{ "pickup_burden",  "maximum burden picked up before prompt",
+						20, SET_IN_GAME },
 	{ "pickup_types", "types of objects to pick up automatically",
 						MAXOCLASSES, SET_IN_GAME },
-	{ "race",     "your starting race (e.g., Human, Elf)", PL_CSIZ, DISP_IN_GAME },
+	{ "race",     "your starting race (e.g., Human, Elf)",
+						PL_CSIZ, DISP_IN_GAME },
 	{ "role",     "your starting role (e.g., Barbarian, Valkyrie)",
 						PL_CSIZ, DISP_IN_GAME },
-	{ "scores",   "the parts of the score list you wish to see", 32, SET_IN_GAME },
+	{ "scores",   "the parts of the score list you wish to see",
+						32, SET_IN_GAME },
 #ifdef MSDOS
-	{ "soundcard", "type of sound card to use", 20, SET_IN_FILE},
+	{ "soundcard", "type of sound card to use", 20, SET_IN_FILE },
 #endif
 	{ "suppress_alert", "suppress alerts about version-specific features",
-						6, SET_IN_GAME},
-	{ "traps",    "the symbols to use in drawing traps", MAXTCHARS+1, SET_IN_FILE },
+						8, SET_IN_GAME },
+	{ "traps",    "the symbols to use in drawing traps",
+						MAXTCHARS+1, SET_IN_FILE },
 #ifdef MAC
-	{"use_stone", "use stone background patterns", 8, SET_IN_FILE},
+	{"use_stone", "use stone background patterns", 8, SET_IN_FILE },
 #endif
 #ifdef MSDOS
 	{ "video",    "method of video updating", 20, DISP_IN_GAME },
 #endif
 #ifdef VIDEOSHADES
-	{ "videocolors", "color mappings for internal screen routines", 40, DISP_IN_GAME},
-	{ "videoshades", "gray shades to map to black/gray/white", 32, DISP_IN_GAME },
+	{ "videocolors", "color mappings for internal screen routines",
+						40, DISP_IN_GAME },
+	{ "videoshades", "gray shades to map to black/gray/white",
+						32, DISP_IN_GAME },
 #endif
-	{ "windowtype", "windowing system to use", WINTYPELEN, DISP_IN_GAME},
+	{ "windowtype", "windowing system to use", WINTYPELEN, DISP_IN_GAME },
 	{ (char *)0, (char *)0, 0 }
 };
 
@@ -1809,7 +1828,7 @@ doset()
 	if (biggest_name > 30) biggest_name = 30;
 	Sprintf(fmtstr_doset_add_menu, "%%s%%-%ds [%%s]", biggest_name);
 
-	/* deliberately put `name',`role',`race', `gender' first */
+	/* deliberately put `name', `role', `race', `gender' first */
 	doset_add_menu(tmpwin, "name", indexoffset);
 	doset_add_menu(tmpwin, "role", indexoffset);
 	doset_add_menu(tmpwin, "race", indexoffset);
@@ -1827,7 +1846,8 @@ doset()
 		    	    	continue;
 		    	else
 				doset_add_menu(tmpwin, compopt[i].name,
-					(pass == DISP_IN_GAME) ? indexoffset : boolcount);
+					(pass == DISP_IN_GAME) ?
+					indexoffset : boolcount);
 		}
 	end_menu(tmpwin, "Set what options?");
 	need_redraw = FALSE;
@@ -1849,7 +1869,8 @@ doset()
 		    /* compound option */
 		    opt_indx -= boolcount;
 
-		    if (!special_handling(compopt[opt_indx].name, setinitial, fromfile)) {
+		    if (!special_handling(compopt[opt_indx].name, setinitial,
+								fromfile)) {
 			Sprintf(buf, "Set %s to what?", compopt[opt_indx].name);
 			getlin(buf, buf2);
 			Sprintf(buf, "%s:%s", compopt[opt_indx].name, buf2);
@@ -1941,11 +1962,12 @@ char *buf;
 	buf[0] = '\0';
 	if (!strcmp(optname,"align"))
 		Sprintf(buf, "%s",
-			(flags.initalign < 0) ? none : aligns[flags.initalign].adj );
+		    (flags.initalign < 0) ? none : aligns[flags.initalign].adj);
 	else if (!strcmp(optname, "catname")) 
 		Sprintf(buf, "%s", catname[0] ? catname : none );
 	else if (!strcmp(optname, "disclose")) 
-		Sprintf(buf, "%s", flags.end_disclose[0] ? flags.end_disclose : "all" );
+		Sprintf(buf, "%s",
+			flags.end_disclose[0] ? flags.end_disclose : "all" );
 	else if (!strcmp(optname, "dogname")) 
 		Sprintf(buf, "%s", dogname[0] ? dogname : none );
 	else if (!strcmp(optname, "dungeon"))
@@ -1956,7 +1978,7 @@ char *buf;
 		Sprintf(buf, "%s", pl_fruit);
 	else if (!strcmp(optname, "gender"))
 		Sprintf(buf, "%s",
-			(flags.initgend < 0) ? none : genders[flags.initgend].adj );
+		    (flags.initgend < 0) ? none : genders[flags.initgend].adj );
 	else if (!strcmp(optname, "horsename")) 
 		Sprintf(buf, "%s", horsename[0] ? horsename : none);
 	else if (!strcmp(optname, "menustyle")) 
@@ -1988,7 +2010,7 @@ char *buf;
 	else if (!strcmp(optname, "msghistory"))
 		Sprintf(buf, "%u", iflags.msg_history);
 	else if (!strcmp(optname, "name"))
-		Sprintf(buf, "%s",plname);
+		Sprintf(buf, "%s", plname);
 	else if (!strcmp(optname, "objects"))
 		Sprintf(buf, "%s", to_be_done);
 	else if (!strcmp(optname, "packorder")) {
@@ -2000,8 +2022,8 @@ char *buf;
 		Sprintf(buf, "%s", get_color_string());
 #endif
 	else if (!strcmp(optname, "pettype")) 
-		Sprintf(buf, "%s",
-	       (preferred_pet == 'c') ? "cat" : (preferred_pet == 'd') ? "dog" : "random" );
+		Sprintf(buf, "%s", (preferred_pet == 'c') ? "cat" :
+				(preferred_pet == 'd') ? "dog" : "random" );
 	else if (!strcmp(optname, "pickup_burden"))
 		Sprintf(buf, "%s", burdentype[flags.pickup_burden] );
 	else if (!strcmp(optname, "pickup_types")) {
@@ -2010,13 +2032,13 @@ char *buf;
 	     }
 	else if (!strcmp(optname, "race"))
 		Sprintf(buf, "%s",
-			(flags.initrace < 0) ? none : races[flags.initrace].noun );
+		    (flags.initrace < 0) ? none : races[flags.initrace].noun );
 	else if (!strcmp(optname, "role")) 
 		Sprintf(buf, "%s",
-			(flags.initrole < 0) ? none : roles[flags.initrole].name.m);
+		    (flags.initrole < 0) ? none : roles[flags.initrole].name.m);
 	else if (!strcmp(optname, "scores")) {
-		Sprintf(buf, "%d top/%d around%s",
-			flags.end_top, flags.end_around, flags.end_own ? "/own" : "");
+		Sprintf(buf, "%d top/%d around%s", flags.end_top,
+				flags.end_around, flags.end_own ? "/own" : "");
 	     }
 #ifdef MSDOS
 	else if (!strcmp(optname, "soundcard"))
@@ -2040,11 +2062,13 @@ char *buf;
 		Sprintf(buf, "%s-%s-%s", shade[0],shade[1],shade[2]);
 	else if (!strcmp(optname, "videocolors"))
 		Sprintf(buf, "%d-%d-%d-%d-%d-%d-%d-%d-%d-%d-%d-%d",
-			ttycolors[CLR_RED], ttycolors[CLR_GREEN], ttycolors[CLR_BROWN],
-			ttycolors[CLR_BLUE], ttycolors[CLR_MAGENTA], ttycolors[CLR_CYAN],
+			ttycolors[CLR_RED], ttycolors[CLR_GREEN],
+			ttycolors[CLR_BROWN], ttycolors[CLR_BLUE],
+			ttycolors[CLR_MAGENTA], ttycolors[CLR_CYAN],
 			ttycolors[CLR_ORANGE], ttycolors[CLR_BRIGHT_GREEN],
 			ttycolors[CLR_YELLOW], ttycolors[CLR_BRIGHT_BLUE],
-			ttycolors[CLR_BRIGHT_MAGENTA], ttycolors[CLR_BRIGHT_CYAN]);
+			ttycolors[CLR_BRIGHT_MAGENTA],
+			ttycolors[CLR_BRIGHT_CYAN]);
 #endif /* VIDEOSHADES */
 	else if (!strcmp(optname, "windowtype"))
 		Sprintf(buf, "%s", windowprocs.name);
