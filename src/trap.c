a9b67f2ad9e36465ee37766fe73d83a1d622a14f
Russell Harmon <russ@eatnumber1.com>
2010-05-10 07:32:09 -0400

    Fixes random crashes when acid drops into water
    Dropping potions of acid or dipping them into water leads to random
    crashes. This patch fixes this and the following official bugs:
    
    C343-19 Dipping acid in a fountain may not destroy the acid.
    C343-179 If a monster is killed or tamed while over water (or by a
    drawbridge) while carrying a potion of acid, the game may panic.
    C343-268 Used up potion of acid may end up in bones file.
    
    Retrieved from http://bilious.homelinux.org/?340

--- src/trap.c
+++ src/trap.c
@@ -2689,12 +2689,15 @@ xchar x, y;
     return retval;
 }
 
-void
+/* returns TRUE if obj is destroyed */
+boolean
 water_damage(obj, force, here)
 register struct obj *obj;
 register boolean force, here;
 {
 	struct obj *otmp;
+	struct obj *obj_original = obj;
+	boolean obj_destroyed = FALSE;
 
 	/* Scrolls, spellbooks, potions, weapons and
 	   pieces of armor may get affected by the water */
@@ -2735,6 +2738,7 @@ register boolean force, here;
 				/* damage player/monster? */
 				pline("A potion explodes!");
 				delobj(obj);
+				obj_destroyed = (obj == obj_original);
 				continue;
 			} else if (obj->odiluted) {
 				obj->otyp = POT_WATER;
@@ -2751,7 +2755,9 @@ register boolean force, here;
 			   (uarmc->cursed && !rn2(3)))
 				obj->oeroded++;
 		}
+		obj_destroyed = FALSE;
 	}
+	return obj_destroyed;
 }
 
 /*
