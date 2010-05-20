034fce5714b5c23dadab55d50b7ccddcb07d654d
William Orr <will@worrbase.com>
2010-05-20 15:45:16 -0400

    Misc Potter bugfixes
    - He's never referred to as "the Potter"
    - Corrected spelling mistake when he becomes violent
    - He's also a guy now

--- src/do_name.c
+++ src/do_name.c
@@ -791,6 +791,10 @@ boolean called;
 	    return strcpy(buf, name);
 	}
 
+	if (strncmp("Potter", mtmp->data->mname, 7) == 0)
+		article = ARTICLE_NONE;
+	
+
 	/* Shopkeepers: use shopkeeper name.  For normal shopkeepers, just
 	 * "Asidonhopo"; for unusual ones, "Asidonhopo the invisible
 	 * shopkeeper" or "Asidonhopo the blue dragon".  If hallucinating,
