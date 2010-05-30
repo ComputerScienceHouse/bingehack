eddd0860d0b6f389182d9d02f3be6e37db5706ad
William Orr <will@worrbase.com>
2010-05-30 15:11:57 -0400

    Changed Delphi in detect code to Potter's House

--- src/detect.c
+++ src/detect.c
@@ -780,7 +780,7 @@ static const struct {
     const char *what;
     d_level *where;
 } level_detects[] = {
-  { "Delphi", &oracle_level },
+  { "Potter's House", &oracle_level },
   { "Medusa's lair", &medusa_level },
   { "a castle", &stronghold_level },
   { "the Wizard of Yendor's tower", &wiz1_level },
