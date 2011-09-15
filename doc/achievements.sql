d723bb194a923a32b2fe87b4d3ad755d1e9f6ca2
Chris Lockfort <clockfort@gmail.com>
2011-09-15 05:06:27 -0400

    Updated SQL structure schema so all current tables can be recreated

--- doc/achievements.sql
+++ doc/achievements.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: db.csh.rit.edu
--- Generation Time: Sep 15, 2011 at 05:00 AM
+-- Generation Time: Sep 15, 2011 at 05:05 AM
 -- Server version: 5.1.49
 -- PHP Version: 5.3.3-7+squeeze3
 
@@ -38,6 +38,67 @@ CREATE TABLE IF NOT EXISTS `achievements` (
   KEY `app_id` (`app_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='maybe add icon and/or sound columns later' AUTO_INCREMENT=27 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `achievement_progress`
+--
+
+DROP TABLE IF EXISTS `achievement_progress`;
+CREATE TABLE IF NOT EXISTS `achievement_progress` (
+  `user_id` int(11) NOT NULL,
+  `achievement_id` int(11) NOT NULL,
+  `progress` int(11) NOT NULL COMMENT 'compare with progress_max  in achievements',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY `user_id` (`user_id`),
+  KEY `achievement_id` (`achievement_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `apps`
+--
+
+DROP TABLE IF EXISTS `apps`;
+CREATE TABLE IF NOT EXISTS `apps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `info_url` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='might change this later to add an abstraction layer to easil' AUTO_INCREMENT=23 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_in_apps`
+--
+
+DROP TABLE IF EXISTS `users_in_apps`;
+CREATE TABLE IF NOT EXISTS `users_in_apps` (
+  `user_id` int(11) NOT NULL,
+  `app_id` int(11) NOT NULL,
+  `app_username` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  UNIQUE KEY `app_username` (`app_username`),
+  KEY `user_id` (`user_id`),
+  KEY `app_id` (`app_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 --
 -- Constraints for dumped tables
 --
@@ -47,6 +108,20 @@ CREATE TABLE IF NOT EXISTS `achievements` (
 --
 ALTER TABLE `achievements`
   ADD CONSTRAINT `achievements_ibfk_1` FOREIGN KEY (`app_id`) REFERENCES `apps` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `achievement_progress`
+--
+ALTER TABLE `achievement_progress`
+  ADD CONSTRAINT `achievement_progress_ibfk_4` FOREIGN KEY (`achievement_id`) REFERENCES `achievements` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `achievement_progress_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users_in_apps`
+--
+ALTER TABLE `users_in_apps`
+  ADD CONSTRAINT `users_in_apps_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `users_in_apps_ibfk_4` FOREIGN KEY (`app_id`) REFERENCES `apps` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
