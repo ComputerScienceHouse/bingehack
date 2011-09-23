601e5d7edf58a89620e4491d1e21862127d1ade6
William Orr <will@worrbase.com>
2011-09-22 22:43:11 -0400

    change to bwrite() prototype

--- include/extern.h
+++ include/extern.h
@@ -1818,7 +1818,7 @@ E void FDECL(savelev, (int,XCHAR_P,int));
 E void FDECL(bufon, (int));
 E void FDECL(bufoff, (int));
 E void FDECL(bflush, (int));
-E void FDECL(bwrite, (int,genericptr_t,unsigned int));
+E void FDECL(bwrite, (genericptr_t,unsigned int, const char*));
 E void FDECL(bclose, (int));
 E void FDECL(savefruitchn, (int,int));
 E void NDECL(free_dungeons);
